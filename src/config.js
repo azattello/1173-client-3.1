cb175beaf16b4061d0d777aff66dbff1fa49a228
Abdykali Azat <azat.abdykalli@gmail.com>
2024-06-18 15:55:01 +0500

    1173 client 3.1

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://1173-cargo.kz:3001',
+    
+    nameCargo: '1173-Cargo'
+  };
+  
+  
+export default config;
+  
